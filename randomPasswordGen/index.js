0167cd801e6140322ce1e79b2c670e9fe5e330c5
Galileo <150097868+iBlueKnight-92@users.noreply.github.com>
2024-07-29 17:52:49 -0600

    Project 2 Random Password Generator added new features and the possibility to add upper case letters, numbers and symbols

--- randomPasswordGen/index.js
+++ randomPasswordGen/index.js
@@ -1,15 +1,36 @@
-const pwd = "";
+//Random Password Generator
 
 function randomPassword(){
-    const length = document.getElementById("numberOfDice").value;
+    const length = document.getElementById("passwordLength").value;
     const randomPwd = document.getElementById("passwordContainer");
+    const includeUpper = document.getElementById("upperCase").checked;
+    const includeNumbers = document.getElementById("numbers").checked;
+    const includeSymbols = document.getElementById("symbols").checked;
 
+    const lowerCase = "abcdefghijklmnñopqrstuvwxyz";
+    const upperCase = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
+    const numbers = "0123456789";
+    const symbols = "!#$%&/(){}|°";
+
+    let allowedChars = "";
     let password = "";
+
+    allowedChars += lowerCase;
+    includeUpper ? allowedChars += upperCase : "";
+    includeNumbers ? allowedChars += numbers : "";
+    includeSymbols ? allowedChars += symbols : "";
+    
+
+    console.log(allowedChars)
+
     for(let i = 0; i < length; i++){
-        const value = Math.floor(Math.random() * 26) + 97;
-        const text = String.fromCharCode(value);
-        password += text;
+        const value = Math.floor(Math.random() * allowedChars.length);
+        password += allowedChars[value];
         
     }
-    randomPwd.textContent = `Your Password is = ${password}`
+
+    randomPwd.textContent = `Your Password is = ${password}`;
+    
 }
+
+
